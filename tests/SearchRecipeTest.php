d30def8d95a971b27a96ffb1fe6d162ad0d2f8ea
qmlgame <qmlgame@gmail.com>
2026-03-24 09:26:40 -0400

    Update SearchRecipeTest.php

--- tests/SearchRecipeTest.php
+++ tests/SearchRecipeTest.php
@@ -10,7 +10,7 @@ final class SearchRecipeTest extends TestCase
 
     protected function setUp(): void
     {
-        $this->sourceFile = __DIR__ . '/../Sprint Planning/Code/recipe_creation.php';
+        $this->sourceFile = __DIR__ . '/../Sprint Planning/Code/recipe.php';
         $this->assertFileExists($this->sourceFile, 'Update $sourceFile to the real search page path.');
     }
 
