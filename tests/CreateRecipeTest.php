d0f0a385f2a392478d4269170400a99252906084
qmlgame <qmlgame@gmail.com>
2026-03-23 23:16:41 -0400

    Update CreateRecipeTest.php

--- tests/CreateRecipeTest.php
+++ tests/CreateRecipeTest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
 
-final class CreateRecipeTest.php extends TestCase
+final class CreateRecipeTest extends TestCase
 {
     private string $sourceFile;
 
