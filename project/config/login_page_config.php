a2dc9ff8cb4d20a637612487de83d3b2cc852993
Charles <charlesdahan5@gmail.com>
2026-04-13 16:46:39 -0400

    Change database connection password
    Updated database connection password for login page configuration.

--- project/config/login_page_config.php
+++ project/config/login_page_config.php
@@ -17,7 +17,7 @@ define('BASE_URL', get_base_url());
 // Simple config file (connects to the database), don't know why it's called login_page_config, can be used everywhere
 $host = "localhost";
 $user = "root";
-$password = "";   // empty for XAMPP on Mac usually
+$password = "Tiopwsric133";   // empty for XAMPP on Mac usually
 $database = "users_db"; // or whatever database name you created in phpMyAdmin
 
 $conn = new mysqli($host, $user, $password, $database);
